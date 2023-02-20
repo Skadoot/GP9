80a3cce0595c8cbc28955f1ca37da9c5548ade75
shr27 <shr27@aber.ac.uk>
2023-02-20 02:38:20 +0000

    added most of the move logic including making sure that moves that put the player in check are not legal moves - need to add special moves next

--- dev/20230216/chess_prototype_improved/src/board.java
+++ dev/20230216/chess_prototype_improved/src/board.java
@@ -1,11 +1,8 @@
 import pieces.piece;
 import vector.vector2;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 public class board
 {
@@ -15,11 +12,9 @@ public class board
     //forsyth edwards notation of the board for saving and loading board states.
     private String board_state;
 
-    //map of squares threatened by defending player, updated at the start of each turn.
-    Map<vector2, vector2> threatened_squares;
-
-    //move directions.
-    vector2[] move_directions = { new vector2(0, 1),  new vector2(0, -1), new vector2(1, 0), new vector2(-1, 0), new vector2(1, 1), new vector2(-1, -1), new vector2(1, -1), new vector2(-1, 1)};
+    //both king positions.
+    private vector2 w_king_position;
+    private vector2 b_king_position;
 
     //constructor for the board.
     public board(String initial_board_state)
@@ -41,10 +36,17 @@ public class board
                     if (Character.isDigit(board_state.charAt(i))) {
                         file += board_state.charAt(i);
                     } else {
+                        vector2 board_position = new vector2(file, rank);
                         if (!Character.isLowerCase(board_state.charAt(i))) {
-                            board[file][rank] = new piece('w', new vector2(file, rank), board_state.charAt(i));
+                            board[file][rank] = new piece('w', board_position, board_state.charAt(i));
+                            if(board_state.charAt(i) == 'k'){
+                                w_king_position = board_position;
+                            }
                         } else {
-                            board[file][rank] = new piece('b', new vector2(file, rank), board_state.charAt(i));
+                            board[file][rank] = new piece('b', board_position, board_state.charAt(i));
+                            if(board_state.charAt(i) == 'k'){
+                                b_king_position = board_position;
+                            }
                         }
                         file++;
                     }
@@ -70,6 +72,14 @@ public class board
 
         //set the new square to be the piece.
         board[coordinate.x][coordinate.y] = selected_piece;
+
+        if(selected_piece.get_type() == 'k') {
+            if(selected_piece.get_color() == 'w') {
+                w_king_position = coordinate;
+            } else {
+                b_king_position = coordinate;
+            }
+        }
 
         //update the piece's position.
         selected_piece.set_position(coordinate);
@@ -103,46 +113,125 @@ public class board
     //filtering for legal moves relative to the current board state and current player, called at the start of each turn.
     public void find_legal_moves(char attacking_player)
     {
-        //create a map of threatened squares, so we can filter out differently if we are in check or are going to be in check.
-        create_check_map(attacking_player);
-
         //loop over each piece that is of a particular color
         for (int file = 0; file < 8; file++) {
             for (int rank = 0; rank < 8; rank++) {
+
+                if (get_piece(new vector2(file, rank)) == null) {continue;}
+
                 piece p = board[file][rank];
 
-                if (p.get_type() == ' ') { continue; }
                 if (p.get_color() != attacking_player) { continue; }
 
-                if (p.get_type() == 'p') {
-                    p.set_possible_moves(find_pawn_legal_moves(p));
-                } else if (p.get_type() == 'b') {
-                    p.set_possible_moves(find_bishop_legal_moves(p));
-                } else if (p.get_type() == 'n') {
-                    p.set_possible_moves(find_knight_legal_moves(p));
-                } else if (p.get_type() == 'r') {
-                    p.set_possible_moves(find_rook_legal_moves(p));
-                } else if (p.get_type() == 'q') {
-                    p.set_possible_moves(find_queen_legal_moves(p));
-                } else if (p.get_type() == 'k') {
-                    p.set_possible_moves(find_king_legal_moves(p));
+                switch (p.get_type()) {
+                    case 'p' :
+                        p.set_possible_moves(find_pawn_legal_moves(p, false));
+                        break;
+                    case 'n' :
+                        p.set_possible_moves(find_knight_legal_moves(p, false));
+                        break;
+                    case 'b' :
+                        p.set_possible_moves(find_bishop_legal_moves(p, false));
+                        break;
+                    case 'r' :
+                        p.set_possible_moves(find_rook_legal_moves(p, false));
+                        break;
+                    case 'q' :
+                        p.set_possible_moves(find_queen_legal_moves(p, false));
+                        break;
+                    case 'k' :
+                        p.set_possible_moves(find_king_legal_moves(p));
+                        break;
                 }
             }
         }
     }
 
-    private ArrayList<vector2> find_pawn_legal_moves(piece p) {
+    private ArrayList<vector2> find_pawn_legal_moves(piece p, boolean check_map) {
         ArrayList<vector2> pawn_legal_moves = new ArrayList<>();
+
+        vector2 move = new vector2();
+        move.y = (p.get_color() == 'w') ? 1 : -1;
+
+        vector2 double_advance = new vector2(move.x, move.y * 2);
+
+        vector2 left_attack = new vector2(move.x--, move.y);
+        vector2 right_attack = new vector2(move.x++, move.y);
+
+        if (get_piece(move) == null) {
+            if (check_map) {
+                pawn_legal_moves.add(move);
+            } else {
+                if (is_move_safe(p, move)) {
+                    pawn_legal_moves.add(move);
+                }
+            }
+            if (get_piece(double_advance) == null && !p.has_moved()) {
+                if (check_map) {
+                    pawn_legal_moves.add(double_advance);
+                } else {
+                    if (is_move_safe(p, double_advance)) {
+                        pawn_legal_moves.add(double_advance);
+                    }
+                }
+            }
+        }
+        if (get_piece(left_attack) == null) {
+            if (check_map) {
+                pawn_legal_moves.add(left_attack);
+            } else {
+                if (is_move_safe(p, left_attack)) {
+                    pawn_legal_moves.add(left_attack);
+                }
+            }
+        }
+        if (get_piece(right_attack) == null) {
+            if (check_map) {
+                pawn_legal_moves.add(right_attack);
+            } else {
+                if (is_move_safe(p, right_attack)) {
+                    pawn_legal_moves.add(right_attack);
+                }
+            }
+        }
         return pawn_legal_moves;
     }
 
-    private ArrayList<vector2> find_knight_legal_moves(piece p) {
-        ArrayList<vector2> pawn_knight_moves = new ArrayList<>();
-        return pawn_knight_moves;
+    private ArrayList<vector2> find_knight_legal_moves(piece p, boolean check_map) {
+        vector2[] knight_directions = { new vector2(2, 1),  new vector2(1, 2), new vector2(-2, 1), new vector2(-1, 2), new vector2(2, -1), new vector2(1, -2), new vector2(-2, -1), new vector2(-1, -2)};
+        ArrayList<vector2> knight_legal_moves = new ArrayList<>();
+
+        for (vector2 move_direction : knight_directions) {
+            vector2 move = new vector2();
+
+            move.x = p.get_position().x + move_direction.x;
+            move.y = p.get_position().y + move_direction.y;
+
+            if (move.x > 7 || move.y > 7 || move.x < 0 || move.y < 0) {continue;}
+
+            if (get_piece(move) == null) {
+                if (check_map) {
+                    knight_legal_moves.add(move);
+                } else {
+                    if (is_move_safe(p, move)) {
+                        knight_legal_moves.add(move);
+                    }
+                }
+            } else if (get_piece(move).get_color() != p.get_color()) {
+                if (check_map) {
+                    knight_legal_moves.add(move);
+                } else {
+                    if (is_move_safe(p, move)) {
+                        knight_legal_moves.add(move);
+                    }
+                }
+            }
+        }
+        return knight_legal_moves;
     }
 
-    private ArrayList<vector2> find_bishop_legal_moves(piece p) {
-        vector2[] bishop_directions = { new vector2(1, 1), new vector2(-1, -1), new vector2(1, -1), new vector2(-1, 1)} ;
+    private ArrayList<vector2> find_bishop_legal_moves(piece p, boolean check_map) {
+        vector2[] bishop_directions = { new vector2(1, 1), new vector2(-1, -1), new vector2(1, -1), new vector2(-1, 1)};
         ArrayList<vector2> bishop_legal_moves = new ArrayList<>();
 
         for (vector2 move_direction : bishop_directions) {
@@ -154,11 +243,19 @@ public class board
                 if (move.x > 7 || move.y > 7 || move.x < 0 || move.y < 0) { break; }
 
                 if (get_piece(move) == null) {
-                    bishop_legal_moves.add(move);
-                } else if (!(get_piece(move) == null) && !(get_piece(move).get_color() == p.get_color())) {
-                    bishop_legal_moves.add(move);
+                    if (check_map) {
+                        bishop_legal_moves.add(move);
+                    } else if (is_move_safe(p, move)) {
+                        bishop_legal_moves.add(move);
+                    }
+                } else if (!(get_piece(move).get_color() == p.get_color())) {
+                    if (check_map) {
+                        bishop_legal_moves.add(move);
+                    }  else if (is_move_safe(p, move)) {
+                        bishop_legal_moves.add(move);
+                    }
                     break;
-                } else if (!(get_piece(move) == null) && (get_piece(move).get_color() == p.get_color())) {
+                } else if ((get_piece(move).get_color() == p.get_color())) {
                     break;
                 }
             }
@@ -166,12 +263,13 @@ public class board
         return bishop_legal_moves;
     }
 
-    private ArrayList<vector2> find_rook_legal_moves(piece p) {
-        vector2[] rook_directions = { new vector2(0, 1),  new vector2(0, -1), new vector2(1, 0), new vector2(-1, 0)} ;
+    private ArrayList<vector2> find_rook_legal_moves(piece p, boolean check_map) {
+        vector2[] rook_directions = { new vector2(0, 1),  new vector2(0, -1), new vector2(1, 0), new vector2(-1, 0)};
         ArrayList<vector2> rook_legal_moves = new ArrayList<>();
 
         for (vector2 move_direction : rook_directions) {
             for (int n = 0; n < 7; n++) {
+
                 vector2 move = new vector2();
                 move.x = p.get_position().x + (move_direction.x * n);
                 move.y = p.get_position().y + (move_direction.y * n);
@@ -179,11 +277,19 @@ public class board
                 if (move.x > 7 || move.y > 7 || move.x < 0 || move.y < 0) { break; }
 
                 if (get_piece(move) == null) {
-                    rook_legal_moves.add(move);
-                } else if (!(get_piece(move) == null) && !(get_piece(move).get_color() == p.get_color())) {
-                    rook_legal_moves.add(move);
+                    if (check_map) {
+                        rook_legal_moves.add(move);
+                    } else if (is_move_safe(p, move)) {
+                        rook_legal_moves.add(move);
+                    }
+                } else if (!(get_piece(move).get_color() == p.get_color())) {
+                    if (check_map) {
+                        rook_legal_moves.add(move);
+                    } else if (is_move_safe(p, move)) {
+                        rook_legal_moves.add(move);
+                    }
                     break;
-                } else if (!(get_piece(move) == null) && (get_piece(move).get_color() == p.get_color())) {
+                } else if ((get_piece(move).get_color() == p.get_color())) {
                     break;
                 }
             }
@@ -191,12 +297,13 @@ public class board
         return rook_legal_moves;
     }
 
-    private ArrayList<vector2> find_queen_legal_moves(piece p) {
+    private ArrayList<vector2> find_queen_legal_moves(piece p, boolean check_map) {
         vector2[] queen_directions = { new vector2(0, 1),  new vector2(0, -1), new vector2(1, 0), new vector2(-1, 0), new vector2(1, 1), new vector2(-1, -1), new vector2(1, -1), new vector2(-1, 1)};
         ArrayList<vector2> queen_legal_moves = new ArrayList<>();
 
         for (vector2 move_direction : queen_directions) {
             for (int n = 0; n < 7; n++) {
+
                 vector2 move = new vector2();
                 move.x = p.get_position().x + (move_direction.x * n);
                 move.y = p.get_position().y + (move_direction.y * n);
@@ -204,11 +311,19 @@ public class board
                 if (move.x > 7 || move.y > 7 || move.x < 0 || move.y < 0) { break; }
 
                 if (get_piece(move) == null) {
-                    queen_legal_moves.add(move);
-                } else if (!(get_piece(move) == null) && !(get_piece(move).get_color() == p.get_color())) {
-                    queen_legal_moves.add(move);
+                    if (check_map) {
+                        queen_legal_moves.add(move);
+                    } else if (is_move_safe(p, move)) {
+                        queen_legal_moves.add(move);
+                    }
+                } else if (!(get_piece(move).get_color() == p.get_color())) {
+                    if (check_map) {
+                        queen_legal_moves.add(move);
+                    } else if (is_move_safe(p, move)) {
+                        queen_legal_moves.add(move);
+                    }
                     break;
-                } else if (!(get_piece(move) == null) && (get_piece(move).get_color() == p.get_color())) {
+                } else if ((get_piece(move).get_color() == p.get_color())) {
                     break;
                 }
             }
@@ -221,41 +336,71 @@ public class board
         ArrayList<vector2> king_legal_moves = new ArrayList<>();
 
         for (vector2 move_direction : king_directions) {
+
             vector2 move = new vector2();
+
             move.x = p.get_position().x + (move_direction.x);
             move.y = p.get_position().y + (move_direction.y);
 
-            if (move.x > 7 || move.y > 7 || move.x < 0 || move.y < 0) { break; }
-
+            if (move.x > 7 || move.y > 7 || move.x < 0 || move.y < 0) { continue; }
             if (get_piece(move) == null) {
                 king_legal_moves.add(move);
-            } else if (!(get_piece(move) == null) && !(get_piece(move).get_color() == p.get_color())) {
+            } else if (!(get_piece(move).get_color() == p.get_color())) {
                 king_legal_moves.add(move);
-                break;
-            } else if (!(get_piece(move) == null) && (get_piece(move).get_color() == p.get_color())) {
-                break;
             }
         }
         return king_legal_moves;
     }
 
+    private boolean is_move_safe(piece p, vector2 move) {
+        board b = this;
+        b.move_piece(b.get_piece(p.get_position()), move);
+        return !b.is_in_check(p.get_color());
+    }
+
+    private boolean is_in_check(char player) {
+        Map<vector2, vector2> check_map = create_check_map(player);
+        if (player == 'w' && check_map.containsValue(w_king_position)) {
+            return true;
+        } else return player == 'b' && check_map.containsValue(b_king_position);
+    }
+
     //create the map of 'threatened' squares to determine if they are in check.
-    private void create_check_map(char attacking_player)
+    private Map<vector2, vector2> create_check_map(char player)
     {
         //initialising the map.
-        Map<vector2, vector2> threatened_squares = new HashMap<>();
-
+        Map<vector2, vector2> check_map = new HashMap<>();
         //here we would create the check map.
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (!(board[i][j].get_color() == attacking_player)) {
-                    //add each of the pieces legal moves to the map. (excluding if the move would put the player in check)
-                    //add the vectors to the map.
+        for (int file = 0; file < 8; file++) {
+            for (int rank = 0; rank < 8; rank++) {
+                if (get_piece(new vector2(file, rank)) == null) {continue;}
+                piece p = get_piece(new vector2(file, rank));
+                if (p.get_color() == player) {continue; }
+                ArrayList<vector2> attacked_squares = new ArrayList<>();
+                switch (p.get_type()) {
+                    case 'p' :
+                        attacked_squares = find_pawn_legal_moves(p, true);
+                        break;
+                    case 'n' :
+                        attacked_squares = find_knight_legal_moves(p, true);
+                        break;
+                    case 'b' :
+                        attacked_squares = find_bishop_legal_moves(p, true);
+                        break;
+                    case 'r' :
+                        attacked_squares = find_rook_legal_moves(p, true);
+                        break;
+                    case 'q' :
+                        attacked_squares = find_queen_legal_moves(p, true);
+                        break;
+                }
+                //add the piece's attacked squares to the check map.
+                for (vector2 attacked_square : attacked_squares) {
+                    check_map.put(attacked_square, attacked_square);
                 }
             }
         }
-
         //update the threatened squares.
-        this.threatened_squares = threatened_squares;
+        return check_map;
     }
 }

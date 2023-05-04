f8f735695c2a029687c499e3fc9373ff542328d4
tyw1 <tyw1@aber.ac.uk>
2023-05-04 14:54:45 +0100

    Updated src/ and dev/ with QA changes

--- src/test/java/uk/ac/aber/cs221/group09/logic/PieceTest.java
+++ src/test/java/uk/ac/aber/cs221/group09/logic/PieceTest.java
@@ -97,7 +97,7 @@ class PieceTest {
    }
 
    @Test
-   public void promoteWhiteQueen() {
+   public void testPromoteWhiteQueen() {
       //creates a new game with a white pawn available to be promoted
       Game game = new Game("rnbqkbnP/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNR b KQq - 0 1", "test", false);
 
@@ -223,6 +223,182 @@ class PieceTest {
       Vector2 testPosition = new Vector2(7, 0);
       Assertions.assertEquals('n', game.getGameBoard().getPiece(testPosition).getType());
       Assertions.assertEquals('b', game.getGameBoard().getPiece(testPosition).getColor());
+   }
+
+   @Test
+   public void testMoveAfterPromoteWhiteQueen() {
+      //creates a new game with a white pawn available to be promoted
+      Game game = new Game("rnbqkbnP/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNR b KQq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h8 to a queen
+      game.promote(0);
+
+      //sets the promoted piece as a test piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 7));
+      Vector2 testPosition = new Vector2(7, 3);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbn1/ppppppp1/8/8/7Q/8/PPPPPPP1/RNBQKBNR", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteWhiteRook() {
+      //creates a new game with a white pawn available to be promoted
+      Game game = new Game("rnbqkbnP/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNR b KQq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h8 to a rook
+      game.promote(1);
+
+      //sets the promoted piece as a test piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 7));
+      Vector2 testPosition = new Vector2(7, 3);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbn1/ppppppp1/8/8/7R/8/PPPPPPP1/RNBQKBNR", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteWhiteBishop() {
+      //creates a new game with a white pawn available to be promoted
+      Game game = new Game("rnbqkbnP/pppppp2/8/8/8/8/PPPPPPP1/RNBQKBNR b KQq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h8 to a bishop
+      game.promote(2);
+
+      //sets the promoted piece as a test piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 7));
+      Vector2 testPosition = new Vector2(3, 3);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbn1/pppppp2/8/8/3B4/8/PPPPPPP1/RNBQKBNR", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteWhiteKnight() {
+      //creates a new game with a white pawn available to be promoted
+      Game game = new Game("rnbqkbnP/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNR b KQq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h8 to a knight
+      game.promote(3);
+
+      //sets the promoted piece as a test piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 7));
+      Vector2 testPosition = new Vector2(6, 5);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbn1/ppppppp1/6N1/8/8/8/PPPPPPP1/RNBQKBNR", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteBlackQueen() {
+      //creates a new game with a black pawn available to be promoted
+      Game game = new Game("rnbqkbnr/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNp b Qq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h1 to a queen
+      game.promote(0);
+
+      //sets the promoted piece as a piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 0));
+      Vector2 testPosition = new Vector2(7, 5);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbnr/ppppppp1/7q/8/8/8/PPPPPPP1/RNBQKBN1", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteBlackRook() {
+      //creates a new game with a black pawn available to be promoted
+      Game game = new Game("rnbqkbnr/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNp b Qq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h1 to a rook
+      game.promote(1);
+
+      //sets the promoted piece as a piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 0));
+      Vector2 testPosition = new Vector2(7, 5);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbnr/ppppppp1/7r/8/8/8/PPPPPPP1/RNBQKBN1", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteBlackBishop() {
+      //creates a new game with a black pawn available to be promoted
+      Game game = new Game("rnbqkbnr/ppppppp1/8/8/8/8/PPPPPP2/RNBQKBNp b Qq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h1 to a bishop
+      game.promote(2);
+
+      //sets the promoted piece as a piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 0));
+      Vector2 testPosition = new Vector2(4, 3);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbnr/ppppppp1/8/8/4b3/8/PPPPPP2/RNBQKBN1", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
+   }
+
+   @Test
+   public void testMoveAfterPromoteBlackKnight() {
+      //creates a new game with a black pawn available to be promoted
+      Game game = new Game("rnbqkbnr/ppppppp1/8/8/8/8/PPPPPPP1/RNBQKBNp b Qq - 0 1", "test", false);
+
+      //checks if there is a promotion available
+      game.isPromotionAvailable();
+      //promotes the pawn on h1 to a knight
+      game.promote(3);
+
+      //sets the promoted piece as a piece to move
+      Piece pieceToMove = game.getGameBoard().getPiece(new Vector2(7, 0));
+      Vector2 testPosition = new Vector2(6, 2);
+
+      for (Vector2 currentMove : pieceToMove.getPossibleMoves()) {
+         if (currentMove.getVector2AsBoardNotation().equals(testPosition.getVector2AsBoardNotation())) {
+            game.getGameBoard().movePiece(pieceToMove, testPosition);
+         }
+      }
+      Assertions.assertEquals("rnbqkbnr/ppppppp1/8/8/8/6n1/PPPPPPP1/RNBQKBN1", game.getGameBoard().getForsythEdwardsBoardNotationArrayIndex(0));
    }
 
 }
